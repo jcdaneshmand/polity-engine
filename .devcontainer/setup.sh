8649fd2d7c3157a5b634ee9aae55af0b826cc47c
Jonah Daneshmand <43185678+jcdaneshmand@users.noreply.github.com>
2026-05-27 21:43:46 -0400

    Install @vitejs/plugin-react in devcontainer setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-npm install -D vitest @types/node jsdom @testing-library/react @testing-library/jest-dom @testing-library/user-event
+npm install -D vitest @types/node jsdom @testing-library/react @testing-library/jest-dom @testing-library/user-event @vitejs/plugin-react
